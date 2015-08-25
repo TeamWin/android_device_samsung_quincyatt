b20022a8085a0d182eff1c62cc504a6437c25c4c
Paul Keith <javelinanddart@gmail.com>
2015-08-25 14:07:07 -0500

    quincyatt: Make it CM
    Change-Id: I498e0525b413c6becba8d8b2a29c9fce8405b776
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo candy5_quincyatt-user
-add_lunch_combo candy5_quincyatt-userdebug
-add_lunch_combo candy5_quincyatt-eng
+add_lunch_combo cm_quincyatt-user
+add_lunch_combo cm_quincyatt-userdebug
+add_lunch_combo cm_quincyatt-eng
 
